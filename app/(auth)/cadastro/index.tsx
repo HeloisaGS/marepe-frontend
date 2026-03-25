9c0b9d7f79a7d9d59df5803b375db78b8ad86f7f
Heloisa <heloisagoncalves26@gmail.com>
2026-03-25 12:43:53 -0300

    feat: adicionar tela de escolha de tipo de cadastro

--- app/(auth)/cadastro/index.tsx
+++ app/(auth)/cadastro/index.tsx
@@ -1,22 +1,123 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { router } from 'expo-router';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import LogoMaré from '../../../assets/images/logo.png'; 
 
 export default function CadastroEscolha() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20, marginBottom: 20 }}>
-        Tela de Cadastro 
-      </Text>
+    <View style={styles.container}>
+      {/* Inicío */}
+      <Text style={styles.tituloPrincipal}>Parece que você é novo aqui!</Text>
+      <Text style={styles.subtitulo}>Como você deseja usar o<Text style={{ fontWeight: 'bold' }}> MaréPE</Text>?</Text>
 
-      <Button
-        title="Ir para login"
-        onPress={() => router.push('/login')}
-      />
+      {/* Cards*/}
+      <TouchableOpacity 
+        style={styles.card}
+        onPress={() => router.push('/(auth)/cadastro/cadastro-cliente')}
+      >
+        <View style={styles.quadradoColorido}>
+          <MaterialCommunityIcons name="account-circle" size={50} color="black" />
+        </View>
+        
+        <View style={styles.containerTexto}>
+          <Text style={styles.textoMaior}>Sou banhista e quero criar conta como <Text style={{ fontWeight: 'bold' }}>Cliente</Text>!</Text>
+          <Text style={styles.textoMenor}>Usar o aplicativo para encontrar vendedores na praia.</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={styles.card}
+        onPress={() => router.push('/(auth)/cadastro/cadastro-ambulante')}
+      >
+        <View style={styles.quadradoColorido}>
+          <MaterialCommunityIcons name="walk" size={50} color="black" />
+        </View>
+        
+        <View style={styles.containerTexto}>
+          <Text style={styles.textoMaior}>Sou vendedor e quero criar conta como <Text style={{ fontWeight: 'bold' }}>Ambulante</Text>!</Text>
+          <Text style={styles.textoMenor}>Para dar visibilidade aos meus produtos na praia.</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={styles.card}
+        onPress={() => router.push('/(auth)/cadastro/cadastro-ambulante')}
+      >
+        <View style={styles.quadradoColorido}>
+          <MaterialCommunityIcons name="umbrella-beach" size={50} color="black" />
+        </View>
+        
+        <View style={styles.containerTexto}>
+          <Text style={styles.textoMaior}>Sou vendedor e quero registrar a minha <Text style={{ fontWeight: 'bold' }}>Barraca</Text>!</Text>
+          <Text style={styles.textoMenor}>Para aqueles que já tem ponto fixo e buscam segurança</Text>
+        </View>
+      </TouchableOpacity>
 
-      <Button
-        title="Ir para home (tabs)"
-        onPress={() => router.push('/(tabs)')}
-      />
+      {/* Navegação para a tela inicial*/}
+      <TouchableOpacity style={{ marginTop: 5 }} onPress={() => router.push('/(auth)')}>
+        <Text style={{ color: 'red' }}>Alterar e-mail.</Text>
+      </TouchableOpacity>
+
+      {/* Imagem assets/images/logo.png*/}
+      <View style={styles.containerLogo}>
+        <Image source={LogoMaré} style={styles.logo} resizeMode="contain" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+  },
+  tituloPrincipal: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+
+  },
+  subtitulo: {
+    fontSize: 20,
+    color: '#666',
+    marginBottom: 30,
+  },
+  card: {
+    flexDirection: 'row', // Alinha ícone e texto lado a lado
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 12,
+    padding: 15,
+    marginBottom: 15,
+  },
+  quadradoColorido: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#F5DBB0',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  containerTexto: {
+    flex: 1, // Faz o texto ocupar o resto do espaço
+  },
+  textoMaior: {
+    fontSize: 18,
+    color: '#000',
+  },
+  textoMenor: {
+    fontSize: 14,
+    color: '#555',
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  logo: {
+    height: 120,
+  },
+});
