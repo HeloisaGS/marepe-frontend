1e4eef7f852eb6c3f424429a89f18a4869f23b6a
Isadora <isadoraalbuqueques@gmail.com>
2026-05-06 10:14:27 -0300

    fix ajuste layout ambulante

--- app/(ambulante)/(tabs)/_layout.tsx
+++ app/(ambulante)/(tabs)/_layout.tsx
@@ -1,15 +1,55 @@
 import { Tabs } from 'expo-router';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TabsAmbulanteLayout() {
   return (
-    <Tabs screenOptions={{
-      tabBarStyle: { backgroundColor: '#E95822' }, 
-      tabBarActiveTintColor: '#FFF',
-    }}>
-      <Tabs.Screen name="index" options={{ title: 'Mapa' }} />
-      <Tabs.Screen name="vitrine" options={{ title: 'Vitrine' }} />
-      <Tabs.Screen name="bateria" options={{ title: 'Bateria' }} />
-      <Tabs.Screen name="perfil" options={{ title: 'Perfil' }} />
+    <Tabs 
+      screenOptions={{
+        tabBarStyle: { 
+          backgroundColor: '#E95822', 
+           
+        }, 
+        tabBarActiveTintColor: '#FFF', 
+        tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.6)', 
+        
+      }}
+    >
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: 'Mapa',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map-marker" size={size} color={color} />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="vitrine" 
+        options={{ 
+          title: 'Vitrine',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="folder-outline" size={size} color={color} />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="bateria" 
+        options={{ 
+          title: 'Bateria',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="moon-waning-crescent" size={size} color={color} />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="perfil" 
+        options={{ 
+          title: 'Perfil',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-outline" size={size} color={color} />
+          )
+        }} 
+      />
     </Tabs>
   );
 }
